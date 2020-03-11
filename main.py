a974f55781164f3db3f0d857035fb135d670d936
Kaappo Raivio <kaappo.raivio@gmail.com>
2020-03-11 14:43:15 +0200

    autocommit

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     else:
         mode = "a"
 
-    with open("data.csv", mode) as file:
+    with open("~/science/data.csv", mode) as file:
         for i in range(1080):
             start = time.time()
             file.write(f"{analyze():.2f};")
